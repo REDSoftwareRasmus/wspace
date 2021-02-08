b1740c8fc7ba002511c540d054f2ed70819a59f1
REDSoftwareRasmus <appdev@redge.se>
2021-02-08 07:57:16 +0100

    Add browser actions

--- actions/browser.sh
+++ actions/browser.sh
@@ -1,2 +1,36 @@
-echo "browser"
-echo $@
+#!/bin/bash
+
+load() { 
+    echo Load $1
+}
+
+add() { 
+    echo adding $1
+}
+
+list() { 
+    echo listing...
+}
+
+remove() { 
+    echo removing $@
+}
+
+
+argn=$#
+iarg=$@
+
+case $iarg in
+    add*) 
+        shift; 
+        add $@;;
+    rm*|remove*) 
+        shift;
+        remove $@;;
+    l*|list*) 
+        list;;
+    * ) 
+        load $@;;
+esac
+
+
